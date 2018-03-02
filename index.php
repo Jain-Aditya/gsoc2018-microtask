c3575553e7670e4e1f74ad49e58e9a2421046cf6
Aditya Jain <adi2007jain@gmail.com>
2018-03-02 11:00:03 +0530

    Used unordered list to display the list of articles

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 
-<!DOCTYPE html>
 <html>
-
-<body>
-
-<?php
-$url="https://petscan.wmflabs.org/?format=json&psid=3316252";
-$result = file_get_contents($url);
-$vars = json_decode($result, true);
-$final_result = $vars["*"][0]["a"]["*"];
-
-foreach($final_result as $val)
-{
-	// echo $val=>title;
-	echo $val['title'];
-    echo "<br>";
-}
-
-?>
-
-</body>
+	<body>
+		<ul>
+			<?php
+				$url="https://petscan.wmflabs.org/?format=json&psid=3316252";
+				$result = file_get_contents($url);
+				$vars = json_decode($result, true);
+				$final_result = $vars["*"][0]["a"]["*"];
+				foreach($final_result as $val)
+				{
+					echo "<li>".$val['title']."</li>";
+				}
+			?>
+		</ul>
+	</body>
 </html>
